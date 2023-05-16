c5a8951d0ac64bdf8b348db7256b7ef8b47e123d
Djenery <Djenery1994@gmail.com>
2023-05-16 22:05:51 +0300

    implemented 2 layouts contacts with recyclerView and item user

---
+++ app/src/main/java/com/example/minisocialnetwork/ui/MyContactsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.minisocialnetwork.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.minisocialnetwork.R
+
+class MyContactsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_contacts)
+    }
+}
